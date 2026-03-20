34ad3a9623494f4149fcfbfd0ea743ad08a7d374
Gagan Prajapati <142085091+Gagan3036@users.noreply.github.com>
2026-03-20 08:47:14 +0530

    Testing the CLI commit

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-# Changed line
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+// Changed line
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 
 app.use("/api", require("./routes/shayari"));
 
 app.listen(3000, () => {
-    console.log("Server is listening on http://localhost:3000")
-})
+  console.log("Server is listening on http://localhost:3000");
+});
